2edffc5e5c53d64f64b39267e5dc53adee39442f
Rafael Martins Almeida <157168573+RafaelMartinsAlmeida@users.noreply.github.com>
2025-01-19 22:02:43 -0300

    Add files via upload

--- Projeto 1.1/assets/Home/script.js
+++ Projeto 1.1/assets/Home/script.js
@@ -1,24 +1,3 @@
-// Aguarda o carregamento completo do DOM antes de executar o código
-document.addEventListener("DOMContentLoaded", () => {
-    const navLinks = document.querySelectorAll(".nav-link"); // Seleciona todos os links de navegação do menu
-    const sections = document.querySelectorAll(".content-section"); // Seleciona todas as seções de conteúdo
-
-    // Adiciona um evento de clique a cada link de navegação
-    navLinks.forEach(link => {
-        link.addEventListener("click", (e) => {
-            e.preventDefault(); // Evita o comportamento padrão do link (navegar para outra página)
-
-            // Remove a classe 'active' de todos os links e seções
-            navLinks.forEach(item => item.classList.remove("active"));
-            sections.forEach(section => section.classList.remove("active"));
-
-            // Adiciona a classe 'active' no link clicado e na seção correspondente
-            link.classList.add("active");
-            const targetSection = document.getElementById(link.getAttribute("data-target")); // Obtém a seção alvo pelo atributo data-target do link
-            targetSection.classList.add("active");
-        });
-    });
-});
 
 // Função para exibir uma seção específica com base no ID
 function showSection(id) {
